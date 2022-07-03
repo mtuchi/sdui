0ad94bc8d9f593d6b1338491dbee89f549a19792
mtuchi <mtuchidev@gmail.com>
2022-07-03 15:52:12 +0300

    wip: improve components

---
+++ src/components/Layout.vue
@@ -0,0 +1,26 @@
+<template>
+
+    <body class="font-sans leading-normal tracking-normal">
+        <Header />
+        <div class="container w-full md:max-w-3xl mx-auto pt-20">
+            <div class="w-full px-4 md:px-6 text-xl text-gray-800 leading-normal" style="font-family:Georgia,serif;">
+                <router-view></router-view>
+            </div>
+        </div>
+        <Footer />
+    </body>
+</template>
+
+<script>
+import Header from './Header.vue'
+import Footer from './Footer.vue'
+
+export default {
+    name: 'Layout',
+    components: {
+        Header,
+        Footer
+    }
+}
+</script>
+
---
+++ src/components/NewsCard.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        This is news feed
+    </div>
+</template>
+<script>
+export default {
+    name: 'NewsCard'
+}
+</script>
