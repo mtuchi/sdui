8af8cd22a56b0e162b213bea1198ec912f83dd6e
mtuchi <mtuchidev@gmail.com>
2022-07-03 15:53:02 +0300

    feat: add routes for pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,60 +1,45 @@
-import Vue from 'vue';
-import Router from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 
-// Components
 import Home from '../pages/Index.vue';
 import About from '../pages/About.vue';
 import GDPR from '../pages/GDPR.vue';
 import News from '../pages/News.vue';
+import Os from '../pages/Os.vue';
 
-Vue.use(Router);
-
-const router = new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home,
-    },
-    {
-        path: '/about',
-        name: 'About',
-        component: About,
-      },
-    {
-      // Assuming you're using the default permalink structure for posts
-      path: '/:postSlug',
-      name: 'News',
-      component: News,
-    },
-    {
-      path: '/gdpr',
-      name: 'GDPR',
-      component: GDPR,
-    },
-  ],
-  mode: 'history',
-  base: '',
-
-  // Prevents window from scrolling back to top
-  // when navigating between components/views
-  scrollBehavior(to, from, savedPosition) {
-    if (savedPosition) {
-      return savedPosition;
-    } else {
-      return { x: 0, y: 0 };
-    }
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
   },
-});
+  {
+    path: '/about',
+    name: 'About',
+    component: About,
+  },
+  {
+    // Assuming you're using the default permalink structure for posts
+    path: '/:postSlug',
+    name: 'News',
+    component: News,
+  },
+  {
+    path: '/gdpr',
+    name: 'GDPR',
+    component: GDPR,
+  },
+  {
+    path: '/os',
+    name: 'Os',
+    component: Os,
+  },
+];
 
-router.afterEach((to) => { // (to, from)
-  // Add a body class specific to the route we're viewing
-  let body = document.querySelector('body');
+const router = createRouter({
+  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+  history: createWebHashHistory(),
+  routes, // short for `routes: routes`,
+})
 
-  const slug = !(to.params.postSlug)
-    ? to.params.pageSlug
-    : to.params.postSlug;
-  body.classList.add('vue--page--' + slug);
-});
 
 export default router;
