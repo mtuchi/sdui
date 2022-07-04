9d96a3f76ba4bf2512e998315017e8be81fc735f
mtuchi <mtuchidev@gmail.com>
2022-07-04 20:47:18 +0300

    feat: fetch news from state

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,30 @@
 <template>
-    <NewsCard />
+    <NewsCard :articles="getArticles" />
 </template>
 
 <script>
 import NewsCard from '../components/NewsCard.vue'
-
+import { mapGetters } from 'vuex';
 export default {
     name: 'Home',
     components: {
         NewsCard,
+    },
+    data() {
+        return {
+            articles: []
+        }
+    },
+    computed: {
+        ...mapGetters(["getArticles"]),
+
+
+    },
+    created() {
+        this.$store.dispatch("loadArticles");
+    },
+    methods: {
+
     }
 }
 </script>
