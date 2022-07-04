e624183fd46a8a2db61c26067e102ede1e9a8a5b
mtuchi <mtuchidev@gmail.com>
2022-07-04 20:38:51 +0300

    feat: update comments

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,25 +3,25 @@ module.exports = app => {
   
     var router = require("express").Router();
   
-    // Create a new Tutorial
+    // Create a new Article
     router.post("/", articles.create);
   
-    // Retrieve all Tutorials
+    // Retrieve all Articles
     router.get("/", articles.findAll);
   
-    // Retrieve all published Tutorials
+    // Retrieve all published Articles
     router.get("/published", articles.findAllPublished);
   
-    // Retrieve a single Tutorial with id
+    // Retrieve a single Article with id
     router.get("/:id", articles.findOne);
   
-    // Update a Tutorial with id
+    // Update a Article with id
     router.put("/:id", articles.update);
   
-    // Delete a Tutorial with id
+    // Delete a Article with id
     router.delete("/:id", articles.delete);
   
-    // Create a new Tutorial
+    // Create a new Article
     router.delete("/", articles.deleteAll);
   
     app.use("/api/articles", router);
