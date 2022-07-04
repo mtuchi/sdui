5c0507cbf5948db5752a81f97b8afe435129eb0b
mtuchi <mtuchidev@gmail.com>
2022-07-04 20:45:58 +0300

    feat: add tailwindcss

--- src/main.js
+++ src/main.js
@@ -3,5 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import './assets/index.css'
+
 const app = createApp(App)
 app.use(router).use(store).mount('#app');
