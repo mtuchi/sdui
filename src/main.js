179e5d5ccdde085fdfe42ce46fe12883b660c436
mtuchi <mtuchidev@gmail.com>
2022-07-05 23:29:25 +0300

    feat: add moment.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import { createApp } from 'vue'
+import moment from 'moment';
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 import './assets/index.css'
 
+moment.locale('en');
+
 const app = createApp(App)
+app.config.globalProperties.$moment = moment;
+
 app.use(router).use(store).mount('#app');
