b359ac55fc938a31d281b506658cbde1b21d2029
mtuchi <mtuchidev@gmail.com>
2022-07-05 15:51:04 +0300

    feat: add frontend url to avoid cors

--- api/server.js
+++ api/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-    origin: "http://localhost:8080"
+    origin: ["http://localhost:8080", "https://sdui-seven.vercel.app"]
 };
 
 app.use(cors(corsOptions));
