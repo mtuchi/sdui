b536c22fee7999ed393a228df3b0c3c0a3b4d8e0
mtuchi <mtuchidev@gmail.com>
2022-07-05 05:15:27 +0300

    style: setup tailwindcss

--- tailwind.conf.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-    darkMode: false,
-    plugins: [
-        require('@tailwindcss/forms')
-    ],
-}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {},
+  },
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false,
+  plugins: [
+    require('@tailwindcss/forms')
+  ],
+}
